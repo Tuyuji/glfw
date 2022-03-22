eeb38cbfe411ac2105841e1241d2621af9b67180
Reece Hagan <drogonmar@tuyuji.com>
2022-03-22 04:18:01 -0700

    Fixed GLFW not compiling correctly.

--- premake5.lua
+++ premake5.lua
@@ -10,20 +10,27 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
+
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
 		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c",
+
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
+
+		"src/glfw_config.h",
 	}
 
 	filter "system:linux"
@@ -33,13 +40,22 @@ project "GLFW"
 		
 		files
 		{
+			"src/x11_platform.h",
+			"src/xkb_unicode.h",
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
 			"src/xkb_unicode.c",
+
+			"src/glx_context.c",
+
+			"src/posix_time.h",
+			"src/posix_thread.h",
+			"src/posix_module.c",
 			"src/posix_time.c",
 			"src/posix_thread.c",
-			"src/glx_context.c",
+			
+			
 			"src/egl_context.c",
 			"src/osmesa_context.c",
 			"src/linux_joystick.c"
